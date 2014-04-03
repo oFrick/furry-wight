273664380b98ab05334565ba75cb7b66bbef9e71
Kiss Dániel <danielkiss21@gmail.com>
2014-04-03 22:56:04 +0200

    Fájlrendszer menüben kilépés elem elkészítve

--- Fájlrendszer/src/com/cleverabbit/gui/MainFrame.java
+++ Fájlrendszer/src/com/cleverabbit/gui/MainFrame.java
@@ -1,13 +1,32 @@
 package com.cleverabbit.gui;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
 
 /**
+ * Az alkalmazás ablaka
  * @author Kiss Dániel
  *
  */
 public class MainFrame extends JFrame {
 	
+	private JMenuBar menüsor;
+	private JMenu fájlrendszerMenü;
+	private JMenuItem újFáljrendszer;
+	private JMenuItem betöltFájlrendszer;
+	private JMenuItem mentFájlrendszer;
+	private JMenuItem kilépés;
+	
+	/**
+	 * Az ablakot megvalósító osztály. Tartalmazza az egyes menüket, azok elemeit, illetve az ezekhez tartozó eseménykezelõket.
+	 * @author Kiss Dániel
+	 * @param név - String, az ablak neve
+	 */
 	public MainFrame(String név){
 		super(név);
 		
@@ -16,7 +35,39 @@ public class MainFrame extends JFrame {
 		this.setSize(800, 600); //Ablak mérete
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //Ablakbezárás esemény kezelése
 		
+		loadMenus();
+		
 		this.setVisible(true); //Láthatóva teszem
+		
+		//Eseménykezelõk
+		kilépés.addActionListener(new ActionListener() { //Kilépés
+			
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				System.exit(0);
+				
+			}
+		});
+	}
+	
+	/**
+	 * Az ablak menüinek létrehozása és betöltése
+	 * @author Kiss Dániel
+	 */
+	private void loadMenus(){
+		//Példányosítás
+		menüsor = new JMenuBar();
+		fájlrendszerMenü = new JMenu("Fájlrendszer");
+		újFáljrendszer = new JMenuItem("Új");
+		betöltFájlrendszer = new JMenuItem("Betölt");
+		kilépés = new JMenuItem("Kilépés");
+		
+		//Objektumok "elhejezése"
+		this.setJMenuBar(menüsor);
+		menüsor.add(fájlrendszerMenü);
+		fájlrendszerMenü.add(újFáljrendszer);
+		fájlrendszerMenü.add(betöltFájlrendszer);
+		fájlrendszerMenü.add(kilépés);
 	}
 
 }
