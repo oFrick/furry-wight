2703ec08fdb7b57c44b36243279465141dd1b7c5
Kiss Dániel <danielkiss21@gmail.com>
2014-04-04 01:19:31 +0200

    Fájlok és mappák megjelenítése (hozzáadás és törlés a JTree-ből). Létrehozás és törlés metódus implementációja folyamatban. A JTreeModel és JTreeModelListener lehet, hogy mégsem kell.

--- Fájlrendszer/src/com/cleverabbit/gui/FájlrendszerMain.java
+++ Fájlrendszer/src/com/cleverabbit/gui/FájlrendszerMain.java
@@ -1,6 +1,7 @@
 package com.cleverabbit.gui;
 
 import javax.swing.SwingUtilities;
+import javax.swing.tree.DefaultMutableTreeNode;
 
 /**
  * @author Kiss Dániel
@@ -17,6 +18,18 @@ public class FájlrendszerMain {
 			public void run() {
 				// TODO Auto-generated method stub
 				MainFrame frame = new MainFrame("Fájlrendszer v0.0.1"); //Frame létrehozása
+				
+				//Tesztelõ kód innentõl!!!!!!
+				DefaultMutableTreeNode jegyzetek = new DefaultMutableTreeNode("Jegyzetek"); //mappa vagy fájl: jegyzetek
+				DefaultMutableTreeNode játékok = new DefaultMutableTreeNode("Játékok"); //mappa vagy fájl: játékok
+				DefaultMutableTreeNode opr1 = new DefaultMutableTreeNode("opr_Rodek.pdf"); //mappa vagy fájl: ...Rodek.pdf
+				DefaultMutableTreeNode opr2 = new DefaultMutableTreeNode("operációs rendszerek puska.txt"); //mappa vagy fájl: puska.txt
+				
+				frame.addTreeNode(jegyzetek); //adjuk hozzá a jegyzetek-nevû elemet (ami egyébként mappa lesz) a megjelenítési fa rootjához
+				frame.addTreeNode(opr1, jegyzetek); //A Rodek féle segédletet a jegyzetek nevû mappába rakjuk
+				frame.addTreeNode(opr2, jegyzetek);
+				frame.addTreeNode(játékok);
+
 			}
 		});
 
--- Fájlrendszer/src/com/cleverabbit/gui/MainFrame.java
+++ Fájlrendszer/src/com/cleverabbit/gui/MainFrame.java
@@ -12,7 +12,11 @@ import javax.swing.JMenuItem;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTree;
+import javax.swing.SwingUtilities;
+import javax.swing.event.TreeModelEvent;
+import javax.swing.event.TreeModelListener;
 import javax.swing.tree.DefaultMutableTreeNode;
+import javax.swing.tree.DefaultTreeModel;
 
 /**
  * Az alkalmazás ablaka
@@ -31,6 +35,8 @@ public class MainFrame extends JFrame {
 	private JPanel panel;
 	
 	private JTree explorer;
+	private DefaultMutableTreeNode rootElement;
+	private DefaultTreeModel treemodel;
 	
 	/**
 	 * Az ablakot megvalósító osztály. Tartalmazza az egyes menüket, azok elemeit, illetve az ezekhez tartozó eseménykezelõket.
@@ -55,10 +61,42 @@ public class MainFrame extends JFrame {
 			
 			@Override
 			public void actionPerformed(ActionEvent arg0) {
-				System.exit(0);
+				//System.exit(0);
+				addTreeNode(new DefaultMutableTreeNode("Dani"));
 				
 			}
 		});
+	}
+	
+	/**
+	 * Elhelyez egy - a grafikus megjelenítéshez használt - node-ot (ami lehet könyvtár vagy mappa) a fa struktúrában
+	 * @author Kiss Dániel
+	 * @param what - {@link DefaultMutableTreeNode}, amit be szeretnénk szúrni/elhelyezni
+	 * @param at - {@link DefaultMutableTreeNode}, ahová el szeretnénk helyezni
+	 */
+	public void addTreeNode(DefaultMutableTreeNode what, DefaultMutableTreeNode at){
+		at.add(what);
+		//treemodel.insertNodeInto(what, at, at.getChildCount());
+		SwingUtilities.updateComponentTreeUI(explorer);
+	}
+	
+	/**
+	 * Elhelyez egy - a grafikus megjelenítéshez használt - node-ot a fa struktúra gyökerében
+	 * @author Kiss Dániel
+	 * @param what - {@link DefaultMutableTreeNode}, amit el szeretnénk helyezni
+	 */
+	public void addTreeNode(DefaultMutableTreeNode what){
+		addTreeNode(what, rootElement);
+		
+	}
+	
+	/**
+	 * Törli a fából a grafikus megjelenítéshez használt node-ot
+	 * @author Kiss Dániel
+	 * @param what - {@link DefaultMutableTreeNode}, amit törölni szeretnénk
+	 */
+	public void removeTreeNode(DefaultMutableTreeNode what){
+		what.removeFromParent();
 	}
 	
 	private void loadContent(){
@@ -66,9 +104,40 @@ public class MainFrame extends JFrame {
 		panel = new JPanel(layout);
 		setContentPane(panel);
 		
-		DefaultMutableTreeNode top = new DefaultMutableTreeNode("Root könyvtár");
-		explorer = new JTree(top);
+		rootElement = new DefaultMutableTreeNode("Root könyvtár");
+		explorer = new JTree(rootElement);
 		JScrollPane treeView = new JScrollPane(explorer);
+		explorer.setVisibleRowCount(15);
+		
+		treemodel = new DefaultTreeModel(rootElement);
+		treemodel.addTreeModelListener((new TreeModelListener() {
+			
+			@Override
+			public void treeStructureChanged(TreeModelEvent e) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			@Override
+			public void treeNodesRemoved(TreeModelEvent e) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			@Override
+			public void treeNodesInserted(TreeModelEvent e) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			@Override
+			public void treeNodesChanged(TreeModelEvent e) {
+				// TODO Auto-generated method stub
+				
+			}
+		}));
+		explorer.setEditable(true);
+		
 		
 		//panel.add(explorer);
 		panel.add(treeView);
