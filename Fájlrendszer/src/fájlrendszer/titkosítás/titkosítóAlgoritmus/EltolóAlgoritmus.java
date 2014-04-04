ccff7502639fa639fa244ea0e992cc74b68cd158
Kiss Dániel <danielkiss21@gmail.com>
2014-04-04 17:43:07 +0200

    A titkosítást végző osztályok kezdeti (nem tesztelt) implementációja.

---
+++ Fájlrendszer/src/fájlrendszer/titkosítás/titkosítóAlgoritmus/EltolóAlgoritmus.java
@@ -0,0 +1,29 @@
+package fájlrendszer.titkosítás.titkosítóAlgoritmus;
+
+public final class EltolóAlgoritmus extends TitkosítóAlgoritmus {
+	
+	public EltolóAlgoritmus(){
+		super();
+	}
+
+	@Override
+	public String kódol(String mit, String kulcs) {
+		char[] mitArray = mit.toCharArray();
+		char[] kulcsArray = mit.toCharArray();
+		for(int i=0; i<mitArray.length; i++){
+			mitArray[i] += kulcsArray[i];
+		}
+		return mitArray.toString();
+	}
+
+	@Override
+	public String dekódol(String mit, String kulcs) {
+		char[] mitArray = mit.toCharArray();
+		char[] kulcsArray = mit.toCharArray();
+		for(int i=0; i<mitArray.length; i++){
+			mitArray[i] -= kulcsArray[i];
+		}
+		return null;
+	}
+
+}
