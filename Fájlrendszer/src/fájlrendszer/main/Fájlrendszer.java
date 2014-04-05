4bf4be7ebc8f66b6da07b3458c09e37444c53452
Kiss Dániel <danielkiss21@gmail.com>
2014-04-05 22:15:59 +0200

    osztály létrehozása

---
+++ Fájlrendszer/src/fájlrendszer/main/Fájlrendszer.java
@@ -0,0 +1,20 @@
+package fájlrendszer.main;
+
+import java.util.List;
+
+import javax.swing.tree.DefaultMutableTreeNode;
+
+/**
+ * A fájlok/mappák tárolására, a merevlemez funkcióinak magas szinten történõ megvalósítására szolgáló osztály.
+ * @author Kiss Dániel
+ *
+ */
+public class Fájlrendszer {
+	
+	private List<Fájl> fájlok;
+	
+	public Fájlrendszer(DefaultMutableTreeNode rootNode){
+		
+	}
+
+}
