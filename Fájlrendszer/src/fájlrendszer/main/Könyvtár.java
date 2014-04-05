7717e42cf29751f33934394329290660fb90e15a
Kiss Dániel <danielkiss21@gmail.com>
2014-04-06 01:37:10 +0200

    Könyvtár osztály, lemezkezelő metódusok

---
+++ Fájlrendszer/src/fájlrendszer/main/Könyvtár.java
@@ -0,0 +1,35 @@
+package fájlrendszer.main;
+
+import java.util.List;
+
+import javax.swing.tree.DefaultMutableTreeNode;
+
+public class Könyvtár {
+	
+	private DefaultMutableTreeNode grafikusNode;
+	
+	public Könyvtár(DefaultMutableTreeNode grafikusNode){
+		this.grafikusNode = grafikusNode;
+	}
+	
+	/**A könyvtár betöltése a lemezrõl, azaz egy elmentett fájlrendszer fájlból.<br/><br/>
+	 * 
+	 * <b>Bálint! Ezt te tudnád itt implementálni a dll-ed alapján</b>
+	 * @author Kiss Dániel
+	 */
+	public void betöltLemezrõl(){
+		
+	}
+	
+	/**
+	 * A könyvtár kiírása a lemezre<br/><br/>
+	 * 
+	 * <b>Bálint! Ezt te tudnád itt implementálni a dll-ed alapján</b>
+	 * @author Kiss Dániel
+	 * 
+	 */
+	public void kiírLemezre(){
+		
+	}
+
+}
