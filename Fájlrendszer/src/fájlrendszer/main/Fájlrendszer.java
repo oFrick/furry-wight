8550c48455fe62ff2a7f32ee985c01569b9d54d9
Kiss Dániel <danielkiss21@gmail.com>
2014-04-06 20:35:19 +0200

    Kezdetleges fájlrendszer osztály

--- Fájlrendszer/src/fájlrendszer/main/Fájlrendszer.java
+++ Fájlrendszer/src/fájlrendszer/main/Fájlrendszer.java
@@ -12,8 +12,49 @@ import javax.swing.tree.DefaultMutableTreeNode;
 public class Fájlrendszer {
 	
 	private List<Fájl> fájlok;
+	private List<Könyvtár> mappák;
+	private Könyvtár root;
 	
+	/**
+	 * Konstruktor.<br/><br/>
+	 * 
+	 * Egy teljesen új fájlrendszert készít
+	 * @author Kiss Dániel
+	 * @param rootNode - {@link DefaultMutableTreeNode}, a fa root grafikus eleme
+	 */
 	public Fájlrendszer(DefaultMutableTreeNode rootNode){
+		this(rootNode, true);
+	}
+	
+	/**
+	 * Konstruktor, melyben eldönthetõ, hogy meglévõ, vagy új fájlrendszerrel szeretnénk dolgozni.<br/>
+	 * Meglévõ esetében fájlból be kell tölteni a fájlrendszert
+	 * @author Kiss Dániel
+	 * @param rootNode - {@link DefaultMutableTreeNode}, a fa root grafikus eleme
+	 * @param isÚjfájlrendszer - boolean, új fájlrendszert szeretnénk-e?
+	 */
+	public Fájlrendszer(DefaultMutableTreeNode rootNode, boolean isÚjfájlrendszer){
+		if (isÚjfájlrendszer){
+			root = new Könyvtár(rootNode);
+			könyvtárLemezre(root);
+		}else{
+			root = könyvtárLemezrõl();
+		}
+	}
+	
+	public Könyvtár könyvtárLemezrõl(){
+		return null;
+	}
+	
+	public void könyvtárLemezre(Könyvtár könyvtár){
+		
+	}
+	
+	public Fájl fájlLemezrõl(){
+		return null;
+	}
+	
+	public void fájlLemezre(Fájl fájl){
 		
 	}
 
