9c310c1109a76aea779aeec87d2f9a10b954c01c
Kiss Dániel <danielkiss21@gmail.com>
2014-04-15 20:04:17 +0200

    javadoc komment hozzáadás

--- Fájlrendszer/src/fájlrendszer/titkosítás/Titkosító.java
+++ Fájlrendszer/src/fájlrendszer/titkosítás/Titkosító.java
@@ -18,10 +18,22 @@ public class Titkosító {
 		this.algoritmus = algoritmus;		
 	}
 	
+	/**
+	 * A megadott titkosító algoritmus alapján titkosítja a sztringet
+	 * @author Kiss Dániel
+	 * @param mit- String
+	 * @return String
+	 */
 	public final String titkosít(String mit){
 		return algoritmus.kódol(mit, kulcs);	
 	}
 	
+	/**
+	 * Ezen objektum által titkosított adatot (String) visszafejti a titkosítás elõtti állapotra
+	 * @author Kiss Dániel
+	 * @param mibõl - String
+	 * @return String
+	 */
 	public final String visszanyer(String mibõl){
 		return algoritmus.dekódol(mibõl, kulcs);
 	}
