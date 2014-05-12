9c001a024500933545cad4d42652b5932ba460f7
Kiss Dániel <danielkiss21@gmail.com>
2014-05-12 22:55:20 +0200

    Bemeneti mezős testreszabható popup ablak

--- Fajlrendszer/src/Seged.java
+++ Fajlrendszer/src/Seged.java
@@ -1,5 +1,7 @@
 
 
+import java.awt.TextField;
+
 import javax.swing.JDialog;
 import javax.swing.JOptionPane;
 
@@ -8,5 +10,18 @@ public class Seged {
 		JOptionPane optionpane = new JOptionPane(uzenet, JOptionPane.INFORMATION_MESSAGE);
 		JDialog dialog = optionpane.createDialog(frame, cim);
 		dialog.setVisible(true);
+	}
+	
+	public static String inputPopup(String üzenet, String minta, String cím,  MainFrame frame){
+		TextField szövegmezõ = new TextField(minta);
+		Object[] válaszLehetõség = {üzenet, szövegmezõ}; 
+		Object[] válaszGomb = {"OK", "Mégsem"};
+		
+		JOptionPane optionpane = new JOptionPane(válaszLehetõség, JOptionPane.QUESTION_MESSAGE, JOptionPane.YES_NO_OPTION, null, válaszGomb, válaszGomb[0]);
+		JDialog dialog = optionpane.createDialog(frame, cím);
+		dialog.setVisible(true);
+		
+		return szövegmezõ.getText();
+		
 	}
 }
